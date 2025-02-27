72ea1b8130351d6c11e2b847fe5ec9191b7a4539
Athreya <119722868+akondi-athreya@users.noreply.github.com>
2025-02-27 21:50:01 +0530

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -25,7 +25,7 @@ const greeting = {
   subTitle:
     "A full-stack developer passionate about building scalable and impactful solutions. I thrive on creating seamless systems and focus on building efficient and user-friendly solutions.",
   resumeLink:
-    "https://drive.google.com/file/d/1t7d7Ktpdc1QfaNposhwCJ0Po9aaBBWaA/view?usp=sharing",
+    "https://drive.google.com/file/d/1g2rWTkkMvcvGMPl--VU5HHZewDRjvVMU/view?usp=sharing",
   portfolio_repository: "https://github.com/akondi-athreya/Portfolio",
   githubProfile: "https://github.com/akondi-athreya",
 };
